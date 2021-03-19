8f1117327a3bc6a38352a584ba84404ab7837bae
Oleg Chuy <misterchuy21@gmail.com>
2021-03-19 12:10:07 +0200

    have changed initial loading from source

--- lab_1/initialLoadDataFromSource/load_CSV_fileToDB.js
+++ lab_1/initialLoadDataFromSource/load_CSV_fileToDB.js
@@ -5,41 +5,60 @@ const csv = require('csv-parser');
 const fs = require('fs');
 const LinesBuilder = require('./linesBuilderForSQL');
 
+const linesBuilder = new LinesBuilder();
+
+
+const asyncFunc = async (data, argumentsLine, table) => {
+    
+    const valuesLine = linesBuilder.createValuesLine(data);
+
+    await db.query(`insert into public.${table}(${argumentsLine}) values(${valuesLine})`)
+        .catch(err => {
+            console.log(data);
+            console.log(err);
+            process.exit(0);
+        });
+
+}
+
 const loadData_CSV_FromSource = async (file, tableName) => {
 
     const tableNameLowerCase = tableName.toLowerCase();
-    const linesBuilder = new LinesBuilder();
 
     let argumentsLine = null;
 
-    await fs.createReadStream(`../text_files/${file}.csv`)
-    .pipe(csv())
-    .on('data', async data => {
+    await new Promise((resolve, reject) => {
+        
+        const promises = [];
+        fs.createReadStream(`../text_files/${file}.csv`)
+        .pipe(csv())
+        .on('data', async data => {
 
-        if (argumentsLine === null) {
-            argumentsLine = linesBuilder.createArgumentsLine(data);
-        }
+            if (tableNameLowerCase === 'population' && data['Time'] > 2019) return;
 
-        const valuesLine = linesBuilder.createValuesLine(data);
+            if (argumentsLine === null) {
+                argumentsLine = linesBuilder.createArgumentsLine(data);
+            }
 
-        await db.query(`insert into public.${tableNameLowerCase}(${argumentsLine}) values(${valuesLine})`)
-            .catch(err => console.log(err));
+            const valuesLine = linesBuilder.createValuesLine(data);
+            promises.push(asyncFunc(data, argumentsLine, tableNameLowerCase));
 
         })
-    .on('end', () => {
-        console.log('END')
+        .on('end', async () => {
+            await Promise.all(promises);
+            console.log(`Initial load in public.${tableNameLowerCase} has done!`);
+            resolve();
+        });
     });
 }
 
-const Validator = require('../initialLoadToDataWarehouse/Validator');
-const validator = new Validator();
 (async () => {
-
+    console.time('loading from sources');
     // await loadData_CSV_FromSource('summer', 'Tournaments');
     // await loadData_CSV_FromSource('winter', 'Tournaments');
     // await loadData_CSV_FromSource('WPP2019_TotalPopulationBySex', 'Population');
     // await loadData_CSV_FromSource('nobel_laureates', 'nobel_laureates');
-
+    console.timeEnd('loading from sources');
     // await loadData_CSV_FromSource('population_new_for_insert', 'Population');
     // await loadData_CSV_FromSource('nobel_laureates_new', 'nobel_laureates');
 
